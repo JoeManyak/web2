9a85aacacf831270570ac90d6682518a90b3ae35
JoeManyak <doncrafttv@gmail.com>
2021-11-12 21:07:38 +0200

    adding spinner, ratelimit, fixes

--- api/mail.go
+++ api/mail.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/go-playground/validator/v10"
 	"github.com/microcosm-cc/bluemonday"
 	"io"
 	"log"
@@ -10,12 +12,48 @@ import (
 	"os"
 )
 
+type Response struct {
+	IsOk         bool     `json:"isOk"`
+	ErrorMessage []string `json:"errorMessage"`
+}
+
+const rateLimit = 5
+
+var Visitors = make(map[string]int)
+
 func Handler(w http.ResponseWriter, r *http.Request) {
+	if Visitors[r.Host] > rateLimit {
+		resp, err := json.Marshal(Response{
+			IsOk:         false,
+			ErrorMessage: []string{"Too many requests!"},
+		})
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		_, err = w.Write(resp)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return
+	}
+	Visitors[r.Host]++
 	data, err := io.ReadAll(r.Body)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	ParseMail(data).Send()
+	errors := ParseMail(data).Send()
+	resp := Response{
+		IsOk:         len(errors) == 0,
+		ErrorMessage: errors,
+	}
+	byteResponse, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	_, err = w.Write(byteResponse)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
 
 type EmailSendConfig struct {
@@ -24,11 +62,11 @@ type EmailSendConfig struct {
 }
 
 type MailForm struct {
-	Email           string `json:"email,omitempty"`
-	Name            string `json:"name,omitempty"`
-	Phone           string `json:"phone,omitempty"`
-	Password        string `json:"password,omitempty"`
-	ConfirmPassword string `json:"confirmPassword,omitempty"`
+	Email           string `json:"email,omitempty" validate:"email,required,lte=50"`
+	Name            string `json:"name,omitempty" validate:"gte=3,required,lte=50"`
+	Phone           string `json:"phone,omitempty" validate:"numeric,required,len=12"`
+	Password        string `json:"password,omitempty" validate:"gte=6,lte=18"`
+	ConfirmPassword string `json:"confirmPassword,omitempty" validate:"gte=6,lte=18"`
 	Config          EmailSendConfig
 }
 
@@ -41,9 +79,24 @@ func GetDefaultConfig() EmailSendConfig {
 
 const mime = "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
 
-func (m MailForm) Send() {
+func (m MailForm) Send() []string {
+	validate := validator.New()
+
 	to := os.Getenv("EMAIL_TO")
 	title := os.Getenv("TITLE")
+	err := validate.Struct(m)
+	validationErrors := make([]string, 0)
+	if err != nil {
+		for _, e := range err.(validator.ValidationErrors) {
+			validationErrors = append(validationErrors, fmt.Sprintf("%s is invalid", e.Field()))
+		}
+	}
+	if m.Password != m.ConfirmPassword {
+		validationErrors = append(validationErrors, "Passwords didn’t match")
+	}
+	if len(validationErrors) > 0 {
+		return validationErrors
+	}
 	msg := "To: " + to + "\r\n" +
 		"Subject: " + title + "\r\n" + mime + "\r\n"
 	msg += "Name : " + m.Name + "<br/>"
@@ -52,12 +105,13 @@ func (m MailForm) Send() {
 	msg += "Password : " + m.Password + "<br/>"
 	msg += "Confirm password : " + m.ConfirmPassword
 	msg = bluemonday.UGCPolicy().Sanitize(msg)
-	err := smtp.SendMail("smtp.gmail.com:587",
+	err = smtp.SendMail("smtp.gmail.com:587",
 		smtp.PlainAuth("", m.Config.from, m.Config.pass, "smtp.gmail.com"),
 		m.Config.from, []string{to}, []byte(msg))
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+	return []string{}
 }
 
 func ParseMail(data []byte) MailForm {
---
+++ src/App.js
@@ -0,0 +1,96 @@
+import './App.css';
+import React, {useState} from "react";
+import Loader from "react-loader-spinner";
+
+function App() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+    const [errorMessage, setErrorMessage] = useState([])
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setErrorMessage([])
+        setIsLoading(true)
+        const formData = {
+            name,
+            email,
+            phone,
+            password,
+            confirmPassword
+        };
+        const response = await fetch('http://localhost:3000/api/mail', {
+                method: "POST",
+                body: JSON.stringify(formData),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        )
+        const result = await response.json()
+        if (result.isOk){
+         setErrorMessage(["Message sent"])
+        } else {
+        setErrorMessage(result.errorMessage)
+        }
+        setIsLoading(false)
+    }
+
+    return (
+        <div>
+            <form className="form" onSubmit={handleSubmit}>
+                <div className="main-label">
+                    Registration form
+                </div>
+                <div>
+                    <input className="form-input" type="text" required minLength={1} value={name}
+                           onChange={(e) => setName(e.target.value)}
+                           placeholder="Name"/>
+                </div>
+                <div>
+                    <input className="form-input" type="email" required value={email}
+                           minLength={5}
+                           onChange={(e) => setEmail(e.target.value)}
+                           placeholder="Email"/>
+                </div>
+                <div>
+                    <input className="form-input" type="text" minLength={12}
+                           maxLength={12} required value={phone}
+                           onChange={(e) => {
+                               setPhone(e.target.value.replace(/[^0-9\d]/ig, ""))
+                           }}
+                           placeholder="Phone number"/>
+                </div>
+                <div>
+                    <input className="form-input" type="password" required value={password}
+                           minLength={6}
+                           maxLength={18}
+                           onChange={(e) => setPassword(e.target.value)}
+                           placeholder="Password"/>
+                </div>
+                <div>
+                    <input className="form-input" type="password" required value={confirmPassword}
+                           minLength={6}
+                           maxLength={18}
+                           onChange={(e) => setConfirmPassword(e.target.value)}
+                           placeholder="Confirm password"/>
+                </div>
+                <input className="form-input" type="submit" disabled={isLoading}/>
+                <div className="spinner">
+                    {isLoading ? <Loader
+                        type="Puff"
+                        color="#00BFFF"
+                        height={100}
+                        width={100}
+                        timeout={30000} //3 secs
+                    /> : null}
+                </div>
+                {errorMessage.map(e => <div className="error"> {e}</div>)}
+            </form>
+        </div>
+    )
+}
+
+export default App;
